a9803bc70f7f0852aba15a52f2e062f7f0237da0
Ji Yoon Rhee <angelarhee02@gmail.com>
2024-10-25 01:01:58 +0900

    added a new tag for interactable objects, added to-be-tested code for manual pick-up items; need more implementation

--- cheese-rat-game/Assets/Scripts/Player-related/PlayerMovement.cs
+++ cheese-rat-game/Assets/Scripts/Player-related/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,12 +7,17 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D myRigidBody2D;
     public float moveSpeed = 10;
     private Vector2 movement;
-    public GameObject cheeseObject; 
+    public GameObject cheeseObject;
+
+    private ManualPickupItem _currentUsableItem;
+    private GameObject _currentInteractable;
+    private bool _isInteractable = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        _currentInteractable = null;
+        _currentUsableItem = null;
     }
 
     // Update is called once per frame
@@ -38,10 +44,48 @@ public class PlayerMovement : MonoBehaviour
             {
                 carryingCheese = false;
             }
-        if (Input.GetKey(KeyCode.E) && cheeseObject.GetComponent<Cheese>().collidingWithPlayer) // Pick Up Cheese
+        if (Input.GetKey(KeyCode.E)) // Pick Up Cheese
             {
-                carryingCheese = true;
+                if (cheeseObject.GetComponent<Cheese>().collidingWithPlayer)
+                {
+                    carryingCheese = true;
+                }
+
+                if (_currentInteractable && _isInteractable)
+                {
+                    SetCurrentItem(_currentInteractable);
+                }
             }
+        if (Input.GetKey(KeyCode.F) && _currentUsableItem != null)
+        {
+            _currentUsableItem.UseItem();
+            Destroy(_currentUsableItem);
+        }
         myRigidBody2D.linearVelocity = moveSpeed * movement;
-    }   
+    }
+
+    public void SetCurrentItem(GameObject currentItem) { 
+        if (currentItem)
+        {
+            _currentUsableItem = (ManualPickupItem)currentItem;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Interactable")
+        {
+            _currentInteractable = collision.gameObject;
+            _isInteractable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Interactable")
+        {
+            _currentInteractable = null;
+            _isInteractable = false;
+        }
+    }
 }
