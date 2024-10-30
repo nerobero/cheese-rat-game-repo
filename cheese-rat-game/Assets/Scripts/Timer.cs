3a9590a7714b5eecc29b2685b88d8f2c0d7fadda
gnightmarez <rjchoi100@gmail.com>
2024-10-29 22:59:14 -0400

    win scenes

--- cheese-rat-game/Assets/Scripts/Timer.cs
+++ cheese-rat-game/Assets/Scripts/Timer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class Timer : MonoBehaviour
     void Update()
     {
         remainingTime -= Time.deltaTime;
+        if (remainingTime <= 0)
+        {
+            SceneManager.LoadScene(3);
+            return;
+        }
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
