e2ad8fdf1513f1b2af77f1cc57ab12ae7d88dde1
Steve Han <49125687+stevehan12138@users.noreply.github.com>
2024-11-01 15:05:54 -0400

    finished cheese

--- cheese-rat-game/Assets/Scripts/Player-related/Items/Cheese.cs
+++ cheese-rat-game/Assets/Scripts/Player-related/Items/Cheese.cs
@@ -1,15 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using FMODUnity; 
-using System;
+using FMODUnity;
 
 public class Cheese : MonoBehaviour
 {
     public GameObject cheeseObject;
     public GameObject playerObject;
-
     public bool collidingWithPlayer = false;
+
+    public float speed = 20f;
+    private Vector2 randomDirection;
+    private float moveDuration = 5f;
+    private float timer = 0f;
+    public Rigidbody2D myRigidBody2D;
+    [SerializeField] protected Animator animator;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,6 +21,12 @@ public class Cheese : MonoBehaviour
             collidingWithPlayer = true;
             playerObject = collision.gameObject;
         }
+
+        if (collision.CompareTag("Wall"))
+        {
+            randomDirection = -randomDirection;
+            timer = 0f;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -25,20 +34,41 @@ public class Cheese : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             collidingWithPlayer = false;
-            playerObject = null;
         }
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        if (playerObject.GetComponent<PlayerMovement>().carryingCheese)
+        {
+            animator.SetBool("isWalking", false);
+        } else {
+            animator.SetBool("isWalking", true);
+        }
+
         if (playerObject != null && playerObject.GetComponent<PlayerMovement>().carryingCheese)
         {
             cheeseObject.transform.position = playerObject.transform.position;
          /* FMOD.Studio.EventInstance PickupSound;
             PickupSound = RuntimeManager.CreateInstance ("event:/Oneshots/ping2");
             PickupSound.start(); */
-            RuntimeManager.PlayOneShot ("event:/Oneshots/ping2");   /* This my Bookgang ah code ~Shiori */
-
+            RuntimeManager.PlayOneShot("event:/Oneshots/ping2");   /* This my Bookgang ah code ~Shiori */
+        } else {
+            if (timer < moveDuration)
+            {
+                myRigidBody2D.MovePosition(myRigidBody2D.position + randomDirection * speed * Time.deltaTime);
+                timer += Time.deltaTime;
+                
+            } else {
+                // Stop movement after the duration
+                timer = 0f;
+                randomDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+            }
         }
     }
 }
